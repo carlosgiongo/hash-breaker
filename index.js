77655160c0734167c8f8bd58efddb5e1805e650b
Carlos Giongo <carlosgiongoo@gmail.com>
2022-06-08 14:16:14 -0300

    Finalizado mvp

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
-let ascii = []
-let rounds = [0]
-
-for(var i=32;i<127;++i) ascii.push(String.fromCharCode(i));
-
-for(let i = 0; i < ascii.length; i++){
-    setInterval(() => {
-        if(ascii[i] == "~"){
-            console.log(rounds)
-            rounds[rounds.length - 1]++
-            if(rounds[rounds.length - 1] == 95){
-                rounds.pop()
-                getLastNumber(rounds) 
-                rounds.push(0)
-            }
-        }
-    }, [0])   
-}
+let md5 = require('md5');
+let argv = require('minimist')(process.argv.slice(2));
 
 function getLastNumber (n){
     if(n.length == 0){
@@ -30,3 +14,41 @@ function getLastNumber (n){
         }
     }
 }
+
+function main(){
+    if(!argv["hash"] || !argv["hash-type"]){
+        console.log("Please provide a hash (param: --hash) and hash-type (param: --hash-type)")
+        return
+    } else {
+        let ascii = []
+        let rounds = [0]
+
+        for(var i=32;i<127;++i) ascii.push(String.fromCharCode(i));
+        ascii.reverse()
+
+        for(let i = 0; i < ascii.length; i++){
+            setInterval(async () => {
+                if(ascii[i] == " "){
+                    let converted_array = rounds.map(x => ascii[x])
+                    let hash = md5(converted_array.join(""));
+                    if(hash == argv["hash"]){
+                        console.log("FOUNDED! Hash: " + hash + " | Password: " + converted_array.join(""));
+                        process.exit(1);
+                    }
+                    process.stdout.write(converted_array.join("") + " => " + hash);
+                    setInterval(() => {
+                        process.stdout.cursorTo(0);
+                        rounds[rounds.length - 1]++
+                        if(rounds[rounds.length - 1] == 95){
+                            rounds.pop()
+                            getLastNumber(rounds) 
+                            rounds.push(0)
+                        }
+                    }, 0)
+                }
+            }, [0])   
+        }
+    }
+}
+
+main()
